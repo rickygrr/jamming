d543219d47626130c132906f64c0a0575e3f3fb1
Ricardo Gomez <rgomez@gatech.edu>
2018-08-09 16:42:34 -0600

    pass down search result and render result list

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,20 +7,26 @@ import Playlist from '../Playlist/Playlist';
 import './App.css';
 
 class App extends Component {
-  render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchResults: []
+        }
+    }
+    render() {
     return (
         <div>
             <h1>Ja<span className="highlight">mmm</span>ing</h1>
             <div className="App">
-              <!-- Add a SearchBar component -->
+              <SearchBar />
               <div className="App-playlist">
-                <!-- Add a SearchResults component -->
-                <!-- Add a Playlist component -->
+                <SearchResults searchResults={this.state.searchResults}/>
+                <Playlist />
               </div>
             </div>
       </div>
     );
-  }
+    }
 }
 
 export default App;
